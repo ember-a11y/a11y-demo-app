a9e2479c229b60b43d314f2fefdcfdb74d8407c7
Ben Holmes <mail@benholmes.uk>
2016-04-27 12:31:37 -0700

    Add example for textarea helper

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('examples', function() {
     this.route('input-helper');
+    this.route('textarea-helper');
   });
 });
 
