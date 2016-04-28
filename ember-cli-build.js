ab09f191327d7e146ae532a03aaacc6fd83a821d
melsumner <melaniersumner@gmail.com>
2016-04-27 21:46:07 -0500

    30% extra!
    polished styles, added logo, streamlined color scheme, updated language,
    added labels to inputs.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -19,6 +20,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
 
   return app.toTree();
 };
