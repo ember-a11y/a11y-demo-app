cf45089419907124d97decdd9c26b817a24b0210
Robdel12 <robertdeluca19@gmail.com>
2016-05-15 00:31:24 -0500

    Dry up demo-div component
    Instead of yielding the text we can just pass the `ariaRole` attribute
    and use that in the template also. Since that's the only thing that
    changes between each component we can put it in the components template
    and simplify the app template :)

--- app/components/demo-div.js
+++ app/components/demo-div.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'div',
-	ariaRole: '',
-	classNames: ['demo-div']
+  tagName: 'div',
+  ariaRole: '',
+  classNames: ['demo-div']
 });
