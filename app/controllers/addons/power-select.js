59d6aa1b038d67c47d38fe077c8f146bc96c9a38
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-04-22 11:49:28 +0530

    chore: run ember-modules-codemod

--- app/controllers/addons/power-select.js
+++ app/controllers/addons/power-select.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
+import Controller from '@ember/controller';
 
-export default Ember.Controller.extend({
+export default Controller.extend({
   cities: ['Barcelona', 'London', 'New York', 'Porto'],
   destination: 'London',
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,9 @@
-import Ember from 'ember';
+import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
 
 // app/controller/index.js
-export default Ember.Controller.extend({
-  moment: Ember.inject.service(),
+export default Controller.extend({
+  moment: service(),
   actions: {
     changeDefaultFormat() {
       this.set('moment.defaultFormat', 'MM.DD.YYYY');
